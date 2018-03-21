f8b78dd5a48442fd164c51d60dff9d9d3e434767
kristansmith <getkristan@gmail.com>
2018-03-21 22:52:44 +0100

    Added API key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 request({
-    url:"https://maps.googleapis.com/maps/api/geocode/json?address=1301%20Lombard%20St%20Philadelphia",
+    url:"https://maps.googleapis.com/maps/api/geocode/json?address=1301%20Lombard%20St%20Philadelphia&key=AIzaSyAe3hVNBw42VsOMdzcUPFEN_3fAu7FptiE",
     json:true
 }, (error, response, body) => {
     console.log(body);
